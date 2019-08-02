5920b32da99f550b69eb9afc134980b91e01a6c7
alexHooper <alexmohack@gmail.com>
2019-08-02 20:48:38 +0530

    main app file

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function App () {
+	return (
+		<div>
+			<nav>
+				<h1>Hello a third time!</h1>
+				<ul>
+					<li>Thing 1</li>
+					<li>Thing 2</li>
+					<li>Thing 3</li>
+				</ul>
+			</nav>
+			<main>
+				<p>This is where most of my content will go...</p>
+			</main>
+		</div>
+	)
+}
+
+export default App
