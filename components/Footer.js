64cf2e40107faf97ffaae4ae7ebca1869824afe3
alexHooper <alexmohack@gmail.com>
2019-08-02 20:57:36 +0530

    create footer component

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Footer is the react component
+// footer is the html tag
+
+function Footer () {
+	return (
+		<footer>
+			<h6>This is the footer of our app</h6>
+		</footer>
+	)
+}
+
+export default Footer
