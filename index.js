af13846f46a727f81720964107829b0d9822ddfe
alexHooper <alexmohack@gmail.com>
2019-08-02 20:34:56 +0530

    use functional components

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function myApp () {
+	return (
+		<ul>
+			<li>Apple</li>
+			<li>Banana</li>
+			<li>Mango</li>
+		</ul>
+	)
+}
+
 // ReactDOM takes in two arguments
 // 1. What to render => HTML/Variables
 // 2. Where to render => document
 
-ReactDOM.render(<h1>Hello World</h1>,
-	document.getElementById('root'))
+ReactDOM.render(
+	<myApp />,
+	document.getElementById('root')
+)
