4b2e297d8ce502947e979506bec0f4524f9a5eac
alexHooper <alexmohack@gmail.com>
2019-08-02 23:22:21 +0530

    use inline styling in component

--- todolist/ToDo.js
+++ todolist/ToDo.js
@@ -4,12 +4,16 @@ function ToDo () {
 	const firstTask = 'Write content'
 	const secondTask = 'Run code'
 
+	// two curly braces are used to wrap inline styles because
+	// 1. First wrap is for JSX
+	// 2. Second wrap is for JS
+
   return (
     <div>
       <input type='checkbox' name='check1' />
       <input type='checkbox' name='check2' />
       <input type='checkbox' name='check3' />
-      <p>First Task: {firstTask}</p>
+      <p style={{color: #ffffff}}>First Task: {firstTask}</p>
       <p>Second Task: {secondTask}</p>
     </div>
   )
