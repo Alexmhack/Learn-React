84d6f7abe8e7aca537be23c20233837bfd1ba778
alexHooper <alexmohack@gmail.com>
2019-08-02 20:37:32 +0530

    create seperate file component

---
+++ MyInfo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function MyInfo () {
+	return (
+		<div>
+			<h1>List of fruits</h1>
+			<ul>
+				<li>Apple</li>
+				<li>Banana</li>
+				<li>Mango</li>
+			</ul>
+		</div>
+	)
+}
+
+export default MyInfo
