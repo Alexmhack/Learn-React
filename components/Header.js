a5c22c1397bb6cf9df958a8f34b1de7173749790
alexHooper <alexmohack@gmail.com>
2019-08-02 20:57:46 +0530

    create Header component

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header () {
+	return (
+		<header>
+			<nav>
+				<h1>Hello a third time!</h1>
+				<ul>
+					<li>Thing 1</li>
+					<li>Thing 2</li>
+					<li>Thing 3</li>
+				</ul>
+			</nav>
+		</header>
+	)
+}
+
+export default Header
