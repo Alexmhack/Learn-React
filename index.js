dfbaa70f042e7142a1ca507a30489eabf4c46ac2
alexHooper <alexmohack@gmail.com>
2019-08-02 19:58:49 +0530

    use ReactDOM to render html

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// ReactDOM takes in two arguments
+// 1. What to render => HTML/Variables
+// 2. Where to render => document
+
+ReactDOM.render(<h1>Hello World</h1>,
+	document.getElementById('root'))
