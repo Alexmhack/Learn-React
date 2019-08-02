5df818f37163ce4fabf2d8f7a7ddb1d24395d1b2
alexHooper <alexmohack@gmail.com>
2019-08-03 00:51:31 +0530

    create todo item component

---
+++ todolist/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TodoItem () {
+	return (
+		<div>
+			<input type='checkbox' name='check1' />
+			<p>Placeholder text here</p>
+		</div>
+	)
+}
+
+export default TodoItem
