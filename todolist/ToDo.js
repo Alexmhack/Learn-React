b4a1562ec58bd2f0c3f699a162a8931e1bb800c6
alexHooper <alexmohack@gmail.com>
2019-08-02 21:18:59 +0530

    create todolist component

---
+++ todolist/ToDo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ToDo () {
+  return (
+    <div>
+      <input type='checkbox' name='check1' />
+      <input type='checkbox' name='check2' />
+      <input type='checkbox' name='check3' />
+    </div>
+  )
+}
