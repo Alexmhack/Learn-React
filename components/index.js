83261a8a11d3a5ef2f62f8da1edd1bb131d91443
alexHooper <alexmohack@gmail.com>
2019-08-02 20:40:37 +0530

    move component to its own folder

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import MyInfo from './MyInfo'
+import MyInfo from './components/MyInfo'
 
 // ReactDOM takes in two arguments
 // 1. What to render => HTML/Variables
