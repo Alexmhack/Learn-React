954a883ef469d82b53f50964bcaf7a3f56472b50
alexHooper <alexmohack@gmail.com>
2019-08-02 20:58:00 +0530

    use header and footer components

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import React from 'react'
 
+import Footer from './components/Footer'
+import Header from './components/Header'
+
 function App () {
 	return (
 		<div>
-			<nav>
-				<h1>Hello a third time!</h1>
-				<ul>
-					<li>Thing 1</li>
-					<li>Thing 2</li>
-					<li>Thing 3</li>
-				</ul>
-			</nav>
+			<Header />
 			<main>
 				<p>This is where most of my content will go...</p>
 			</main>
+			<Footer />
 		</div>
 	)
 }
