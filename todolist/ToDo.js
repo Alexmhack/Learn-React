9686f7b46475206544136c1e25b790d899a28b0c
alexHooper <alexmohack@gmail.com>
2019-08-02 22:51:46 +0530

    use jsx notation

--- todolist/ToDo.js
+++ todolist/ToDo.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 function ToDo () {
+	const firstTask = 'Write content'
+	const secondTask = 'Run code'
+
   return (
     <div>
       <input type='checkbox' name='check1' />
       <input type='checkbox' name='check2' />
       <input type='checkbox' name='check3' />
+      <p>First Task: {firstTask}</p>
+      <p>Second Task: {secondTask}</p>
     </div>
   )
 }
