2a709540c60fa790283ac4ce8a2a02c3371fd12a
alexHooper <alexmohack@gmail.com>
2019-08-02 20:37:44 +0530

    import and use component

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function myApp () {
-	return (
-		<ul>
-			<li>Apple</li>
-			<li>Banana</li>
-			<li>Mango</li>
-		</ul>
-	)
-}
+import MyInfo from './MyInfo'
 
 // ReactDOM takes in two arguments
 // 1. What to render => HTML/Variables
 // 2. Where to render => document
 
 ReactDOM.render(
-	<myApp />,
+	<MyInfo />,
 	document.getElementById('root')
 )
