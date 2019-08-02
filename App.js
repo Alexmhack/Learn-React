9a4f86684798a5e99f85b3f6cffafca9b3217211
alexHooper <alexmohack@gmail.com>
2019-08-02 21:05:27 +0530

    use standard format

--- App.js
+++ App.js
@@ -4,15 +4,15 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 
 function App () {
-	return (
-		<div>
-			<Header />
-			<main>
-				<p>This is where most of my content will go...</p>
-			</main>
-			<Footer />
-		</div>
-	)
+  return (
+    <div>
+      <Header />
+      <main>
+        <p>This is where most of my content will go...</p>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
