6bcdb8a8c85144c2015930dba194101996744492
alexHooper <alexmohack@gmail.com>
2019-08-03 07:59:59 +0530

    use contact object to get values"

--- props/ContactCard.js
+++ props/ContactCard.js
@@ -3,10 +3,10 @@ import React from 'react'
 function ContactCard (props) {
 	return (
     <div className="contact-card">
-        <img src="{props.imgUrl}"/>
-        <h3>{props.name}</h3>
-        <p>Phone: {props.phone}</p>
-        <p>Email: {props.email}</p>
+        <img src="{props.contact.imgUrl}"/>
+        <h3>{props.contact.name}</h3>
+        <p>Phone: {props.contact.phone}</p>
+        <p>Email: {props.contact.email}</p>
     </div>
 	)
 }
