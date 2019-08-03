ed41757b0b399537f7a332c9d21515af24803b5f
alexHooper <alexmohack@gmail.com>
2019-08-03 08:00:09 +0530

    pass in contact object

--- App.js
+++ App.js
@@ -9,14 +9,14 @@ import ContactCard from './props/ContactCard'
 function App () {
   return (
     <div className='contact-card'>
-    	<ContactCard name="Mr. Whiskerson" imgUrl="http://placekitten.com/300/200"
-    		phone="(212) 555-1234" email="mr.whiskaz@catnap.meow" />
-    	<ContactCard name="Fluffykins" imgUrl="http://placekitten.com/400/200"
-    		phone="(212) 555-2345" email="fluff@me.com" />
-    	<ContactCard name="Destroyer" imgUrl="http://placekitten.com/400/300"
-    		phone="(212) 555-3456" email="ofworlds@yahoo.com" />
-    	<ContactCard name="Felix" imgUrl="http://placekitten.com/200/100"
-    		phone="(212) 555-4567" email="thecat@hotmail.com" />
+    	<ContactCard contact={{name:"Mr. Whiskerson", imgUrl:"http://placekitten.com/300/200",
+    		phone:"(212) 555-1234", email:"mr.whiskaz@catnap.meow" }} />
+    	<ContactCard contact={{name:"Fluffykins", imgUrl:"http://placekitten.com/300/200",
+    		phone:"(212) 555-1234", email:"fluff@me.com" }} />
+    	<ContactCard contact={{name:"Destroyer", imgUrl:"http://placekitten.com/300/200",
+    		phone:"(212) 555-1234", email:"ofworlds@yahoo.com" }} />
+    	<ContactCard contact={{name:"Felix", imgUrl:"http://placekitten.com/300/200",
+    		phone:"(212) 555-1234", email:"thecat@hotmail.com" }} />
     </div>
   )
 }
