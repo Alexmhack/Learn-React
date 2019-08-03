a6ce6b46e3c9bb712277c18b704ea49e4c0dff42
alexHooper <alexmohack@gmail.com>
2019-08-03 07:43:27 +0530

    create contact card component

---
+++ props/ContactCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ContactCard () {
+	return (
+    <div className="contact-card">
+        <img src="http://placekitten.com/300/200"/>
+        <h3>Mr. Whiskerson</h3>
+        <p>Phone: (212) 555-1234</p>
+        <p>Email: mr.whiskaz@catnap.meow</p>
+    </div>
+	)
+}
+
+export default ContactCard
