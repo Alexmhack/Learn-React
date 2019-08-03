93ae6e994552f37cc1d30d60bc3f56f4f5fa71bd
alexHooper <alexmohack@gmail.com>
2019-08-03 08:18:36 +0530

    another way to hide html

--- jokes/Joke.js
+++ jokes/Joke.js
@@ -5,6 +5,9 @@ function Joke (props) {
     <div>
       {props.question !== undefined ? <h1>Question: {props.question}</h1> : "No question"}
       <p>Punchline: {props.punchline}</p>
+
+      <h3 style={{display: props.question ? "block" : "none"}}>Question: {props.question}</h3>
+      <p>Punchline: {props.punchline}</p>
     </div>
   )
 }
