a9fce43ae991d0a13e0ddd095d6fd09bf89b4063
alexHooper <alexmohack@gmail.com>
2019-08-03 07:51:42 +0530

    use props to pass in properties

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from 'react'
 
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Main from './components/Main'
+import ContactCard from './props/ContactCard'
+
+// import Footer from './components/Footer'
+// import Header from './components/Header'
+// import Main from './components/Main'
 
 function App () {
   return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
+    <div className='contact-card'>
+    	<ContactCard name="Mr. Whiskerson" imgUrl="http://placekitten.com/300/200"
+    		phone="(212) 555-1234" email="mr.whiskaz@catnap.meow" />
+    	<ContactCard name="Fluffykins" imgUrl="http://placekitten.com/400/200"
+    		phone="(212) 555-2345" email="fluff@me.com" />
+    	<ContactCard name="Destroyer" imgUrl="http://placekitten.com/400/300"
+    		phone="(212) 555-3456" email="ofworlds@yahoo.com" />
+    	<ContactCard name="Felix" imgUrl="http://placekitten.com/200/100"
+    		phone="(212) 555-4567" email="thecat@hotmail.com" />
     </div>
   )
 }
 
 export default App
+
+// <Header />
+// <Main />
+// <Footer />
