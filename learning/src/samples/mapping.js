7bcadb75838767d08e6a59213a8a9ec01702d1f4
alexHooper <alexmohack@gmail.com>
2019-08-05 20:41:57 +0530

    use two args with map method

--- learning/src/samples/mapping.js
+++ learning/src/samples/mapping.js
@@ -1,7 +1,13 @@
 const singles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
+// map returns a new array
 const doubles = singles.map(function(num) {
 	return num * 2
 })
 
-console.log(doubles)
+// pass in two args => first is element, second is index of element
+singles.map(function(num, index) {
+	console.log(num, index)
+})
+
+// console.log(doubles)
