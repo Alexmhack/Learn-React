4df7bd88acd40befa909ceb2eae441fc0ffb2f4c
alexHooper <alexmohack@gmail.com>
2019-08-06 17:03:48 +0530

    wrap div in container and row

--- learning/src/jokes/Joke.js
+++ learning/src/jokes/Joke.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Joke (props) {
 	return (
-		<div>
-			{props.question !== undefined ? <h1>{props.question}</h1> : "No Question"}
-			{props.punchline !== undefined ? <p>{props.punchline}</p> : "No Punchline"}
+		<div className={'container'}>
+			<div className={'row'}>
+				<div className={'col-md-12'}>
+					{props.question !== undefined ? <h1>{props.question}</h1> : "No Question"}
+					{props.punchline !== undefined ? <p>{props.punchline}</p> : "No Punchline"}
+				</div>
+			</div>
 		</div>
 	)
 }
