d6ad56265710390ea80a0e8638ceb223269502e1
alexHooper <alexmohack@gmail.com>
2019-08-06 17:24:54 +0530

    map through products data

--- learning/src/products/ProductList.js
+++ learning/src/products/ProductList.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
+import Product from './Product'
+import products from './vshoolProducts'
+
 function ProductList (props) {
+	const productComponents = products.map(product => <Product key={product.id}
+		name={product.name} description={product.description} price={product.price} />)
+
 	return (
     <div className="App py-5">
 			<div className="container">
