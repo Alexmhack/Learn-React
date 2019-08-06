1ae7cc40e395fb8361b239de1012da7384f536bc
alexHooper <alexmohack@gmail.com>
2019-08-06 17:14:59 +0530

    show product details

--- learning/src/products/Product.js
+++ learning/src/products/Product.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function Product (props) {
 	return (
-		<div>
-			<h1></h1>
+		<div className="card mb-4">
+		  <img src="" className="card-img-top" alt={"Product " + props.name} />
+		  <div className="card-body">
+		    <h5 className="card-title">{props.name}</h5>
+		    <h6 className="card-text">{props.price}</h6>
+		    <p className="card-text">{props.description}</p>
+		    <a href="#" className="btn btn-primary">Details</a>
+		  </div>
 		</div>
 	)
 }
