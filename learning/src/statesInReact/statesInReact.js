78b4ecb5745d6250b78a3b12c32086d4d20089bb
alexHooper <alexmohack@gmail.com>
2019-08-06 21:27:52 +0530

    define constructor

--- learning/src/statesInReact/statesInReact.js
+++ learning/src/statesInReact/statesInReact.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 
 // states can only be used in class based component
+// initialized in constructor method of class
+// constructor needs to call super for running base class constructor
+// you can also pass in argument named props to constructor
 
 class statesInReact extends Component {
+	constructor (props) {
+		super(props)
+	}
+
 	render () {
 		return (
 			<div>
