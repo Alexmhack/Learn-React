9ee02cafd23fd3fda71eda85cf5e0f41a093f31b
alexHooper <alexmohack@gmail.com>
2019-08-06 17:13:51 +0530

    remove joke and define product component

--- learning/src/App.js
+++ learning/src/App.js
@@ -3,8 +3,11 @@ import React from 'react';
 // import VideoRecorderSample from './thirdparty/VideoRecorderSample'
 // <VideoRecorderSample />
 
-import Joke from './jokes/Joke'
-import JokesData from './jokes/JokesData'
+// import Joke from './jokes/Joke'
+// import JokesData from './jokes/JokesData'
+
+import products from './products/vshoolProducts'
+import Product from './products/Product'
 
 function App() {
 	// const jokeComponents = JokesData.map(function(joke) {
@@ -12,8 +15,12 @@ function App() {
 	// })
 
 	// short form of above code
-	const jokeComponents = JokesData.map(joke => <Joke key={joke.id} question={joke.question}
-		punchline={joke.punchLine} />)
+	// const jokeComponents = JokesData.map(joke => <Joke key={joke.id} question={joke.question}
+	// 	punchline={joke.punchLine} />)
+	// {jokeComponents}
+
+	const productComponents = products.map(product => <Product key={product.id}
+		name={product.name} description={product.description} price={product.price} />)
 
   return (
     <div className="App py-5">
