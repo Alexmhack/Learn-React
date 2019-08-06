752687c1acfafcbcf8a5a82eaab5fafd25ad05ce
alexHooper <alexmohack@gmail.com>
2019-08-06 21:08:38 +0530

    define render method

--- learning/src/classBased/classComponent.js
+++ learning/src/classBased/classComponent.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
 
 class classComponent extends Component {
-
+	render () {
+		return (
+			<div>
+				<h2>This is the class based component.</h2>
+			</div>
+		)
+	}
 }
+
+export default classComponent
