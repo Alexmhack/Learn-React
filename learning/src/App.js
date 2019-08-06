31fddd25735613652f170df1ab52923cb49240ab
alexHooper <alexmohack@gmail.com>
2019-08-06 17:24:16 +0530

    move to ProductsList component

--- learning/src/App.js
+++ learning/src/App.js
@@ -8,8 +8,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 // import Joke from './jokes/Joke'
 // import JokesData from './jokes/JokesData'
 
-import products from './products/vshoolProducts'
-import Product from './products/Product'
 import ProductList from './products/ProductList'
 
 function App() {
@@ -21,9 +19,6 @@ function App() {
 	// const jokeComponents = JokesData.map(joke => <Joke key={joke.id} question={joke.question}
 	// 	punchline={joke.punchLine} />)
 	// {jokeComponents}
-
-	const productComponents = products.map(product => <Product key={product.id}
-		name={product.name} description={product.description} price={product.price} />)
 
   return (
   	<ProductList />
