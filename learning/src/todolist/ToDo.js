48244a3311b5ef18c5a9ba5c602dca7adf62d60e
alexHooper <alexmohack@gmail.com>
2019-08-06 20:31:54 +0530

    create todo list component

---
+++ learning/src/todolist/ToDo.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import TodoItem from './TodoItem'
+import todoData from './todoData'
+
+function ToDo (props) {
+	const todoComponents = todoData.map(item => <TodoItem key={item.id} id={item.id}
+		text={item.text} completed={item.completed} />)
+
+	return (
+		<div className="container">
+			<div className="row mt-4">
+				<div className="col-md-6 offset-md-3">
+					<h4 className="mt-2 mb-4">Todo List</h4>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-md-6 offset-md-3">
+					{todoComponents}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ToDo
