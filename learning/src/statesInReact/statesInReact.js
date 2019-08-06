95ab3937d3fd45967d95c49913cf99ca892f1a2f
alexHooper <alexmohack@gmail.com>
2019-08-06 21:22:34 +0530

    learn states in react

---
+++ learning/src/statesInReact/statesInReact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+// states can only be used in class based component
+
+class statesInReact extends Component {
+	render () {
+		return (
+			<div>
+				<h4>States is a feature of React.</h4>
+			</div>
+		)
+	}
+}
+
+export default statesInReact
