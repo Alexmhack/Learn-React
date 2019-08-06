d32765613c4eecbf25d9fe0a843420076cc127fa
alexHooper <alexmohack@gmail.com>
2019-08-06 20:36:18 +0530

    reduce code

--- learning/src/todolist/TodoItem.js
+++ learning/src/todolist/TodoItem.js
@@ -1,27 +1,22 @@
 import React from 'react'
 
 function TodoItem (props) {
+	const completed = props.completed
+
 	return (
 		<div className="card mb-4">
 			<div className="card-body">
 				<h5 className="card-title">
-					{props.completed ? <s><p>{props.text}</p></s> : <p>{props.text}</p>}
+					{completed ? <s><p>{props.text}</p></s> : <p>{props.text}</p>}
 				</h5>
 
 				<div className="input-group mb-3">
 				  <div className="input-group-prepend">
 				    <div className="input-group-text">
-				      {props.completed ?
-				      	<input type='checkbox'
-				      		aria-label="Checkbox for following text input"
-				      		name='check1'
-				      		checked={true} />
-				      	:
-				      	<input type='checkbox'
-				      		aria-label="Checkbox for following text input"
-				      		name='check1'
-				      		checked={false} />
-				      }
+			      	<input type='checkbox'
+			      		aria-label="Checkbox for following text input"
+			      		name='check1'
+			      		checked={completed} />
 				    </div>
 				  </div>
 				</div>
