686d3de3c9c7220dfcd300f235dc0a0b51f844cc
alexHooper <alexmohack@gmail.com>
2019-08-06 18:19:40 +0530

    change display and add product detail link

--- learning/src/products/Product.js
+++ learning/src/products/Product.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 function Product (props) {
 	return (
 		<div className="card mb-4">
-		  <img src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c06017872.png" className="card-img-top" alt={"Product " + props.name} />
+		  <img height="500" width="100" src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c06017872.png" className="card-img-top" alt={"Product " + props.name} />
 		  <div className="card-body">
 		    <h5 className="card-title">{props.name}</h5>
-		    <h6 className="card-text">{props.price}</h6>
+		    <h5 className="card-title">ID: {props.id}</h5>
+		    <h6 className="card-text">Price: {props.price}</h6>
 		    <p className="card-text">{props.description}</p>
-		    <a href="#" className="btn btn-primary">Details</a>
+		    <Link maintainScrollPosition={true} to={`/product/${props.id}/`} className="btn btn-primary">
+		    	Details
+		    </Link>
 		  </div>
 		</div>
 	)
