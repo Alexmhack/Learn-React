62f58df4740d775b5c12df7ea2a8257cb9478e29
alexHooper <alexmohack@gmail.com>
2019-08-06 17:25:07 +0530

    add sample image source

--- learning/src/products/Product.js
+++ learning/src/products/Product.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Product (props) {
 	return (
 		<div className="card mb-4">
-		  <img src="" className="card-img-top" alt={"Product " + props.name} />
+		  <img src="https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c06017872.png" className="card-img-top" alt={"Product " + props.name} />
 		  <div className="card-body">
 		    <h5 className="card-title">{props.name}</h5>
 		    <h6 className="card-text">{props.price}</h6>
