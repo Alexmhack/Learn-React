bded79412007d7dd3f64197223faa1ccb56420a2
alexHooper <alexmohack@gmail.com>
2019-08-07 20:58:24 +0530

    convert into react component

--- learning/src/todolist/ToDo.js
+++ learning/src/todolist/ToDo.js
@@ -1,25 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import TodoItem from './TodoItem'
 import todoData from './todoData'
 
-function ToDo (props) {
-	const todoComponents = todoData.map(item => <TodoItem key={item.id} item={item} />)
-
-	return (
-		<div className="container">
-			<div className="row mt-4">
-				<div className="col-md-6 offset-md-3">
-					<h4 className="mt-2 mb-4">Todo List</h4>
+class ToDo extends Component {
+	render () {
+		const todoComponents = todoData.map(item => <TodoItem key={item.id} item={item} />)
+		return (
+			<div className="container">
+				<div className="row mt-4">
+					<div className="col-md-6 offset-md-3">
+						<h4 className="mt-2 mb-4">Todo List</h4>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-6 offset-md-3">
+						{todoComponents}
+					</div>
 				</div>
 			</div>
-			<div className="row">
-				<div className="col-md-6 offset-md-3">
-					{todoComponents}
-				</div>
-			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default ToDo
