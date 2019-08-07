578add1ae83aaa3c794f2f12c56a36cda1866f9b
alexHooper <alexmohack@gmail.com>
2019-08-07 23:53:33 +0530

    add another route

--- learning/src/samples/SamplesRoutes.js
+++ learning/src/samples/SamplesRoutes.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Route } from 'react-router-dom'
 
 import EventHandling from './EventHandling'
+import ChangingState from './ChangingState'
 
 function SamplesRoutes () {
 	return (
-		<Route path='/samples/event' component={EventHandling} />
+    <Fragment>
+  		<Route path='/samples/event' component={EventHandling} />
+      <Route path='/samples/change-state' component={ChangingState} />
+    </Fragment>
 	)
 }
 
