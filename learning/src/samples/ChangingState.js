d08d05826c0a47b170cdada350907267364e88a5
alexHooper <alexmohack@gmail.com>
2019-08-08 00:29:39 +0530

    increment state on button click

--- learning/src/samples/ChangingState.js
+++ learning/src/samples/ChangingState.js
@@ -3,20 +3,28 @@ import React, { Component } from 'react'
 class ChangingState extends Component {
   constructor (props) {
     super(props)
+    this.handleClick = this.handleClick.bind(this)
     this.state = {
       count: 0
     }
   }
 
+  handleClick () {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+
   render () {
     let count = this.state.count
-    count = 12
 
     return (
       <div className='py-5 text-center'>
         <h1>{count}</h1>
         <br />
-        <button onClick={e => console.log(count + ' is the value of count')}
+        <button onClick={this.handleClick}
           className='btn btn-primary'>Change</button>
       </div>
     )
