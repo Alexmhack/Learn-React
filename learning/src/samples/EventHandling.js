672b7ef30d7dc6441d9d38b357c0924bca566d3a
alexHooper <alexmohack@gmail.com>
2019-08-07 21:35:07 +0530

    create event handling component

---
+++ learning/src/samples/EventHandling.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function EventHandling (props) {
+	return (
+		<div className="pt-5 text-center">
+			<img src="https://www.fillmurray.com/500/400" />
+			<br />
+			<br />
+			<br />
+			<button>Click me</button>
+		</div>
+	)
+}
+
+export default EventHandling
