2e0ccc6f26d37526f6a9f478a8671ab46bb770ea
alexHooper <alexmohack@gmail.com>
2019-08-07 23:53:19 +0530

    render count from state

--- learning/src/samples/ChangingState.js
+++ learning/src/samples/ChangingState.js
@@ -1,3 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+class ChangingState extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
 
+  render () {
+    let count = this.state.count
+    count = 12
+
+    return (
+      <div className='py-5 text-center'>
+        <h1>{count}</h1>
+        <br />
+        <button onClick={e => console.log(count + ' is the value of count')}
+          className='btn btn-primary'>Change</button>
+      </div>
+    )
+  }
+}
+
+export default ChangingState
