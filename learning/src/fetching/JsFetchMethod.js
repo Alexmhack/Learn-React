78744600d6eb5fa81de4ca9b802b42b2b7936a7c
alexHooper <alexmohack@gmail.com>
2019-08-10 01:03:26 +0530

    use Person component and pass in data

--- learning/src/fetching/JsFetchMethod.js
+++ learning/src/fetching/JsFetchMethod.js
@@ -1,18 +1,34 @@
 import React, {Component} from 'react'
 
+import Person from './Person'
+
 class JsFetchMethod extends Component {
+  constructor() {
+    super()
+    this.state = {
+      person: null
+    }
+  }
+
   componentDidMount() {
     const endpoint = "https://swapi.co/api/people/1/"
 
     // global javascript method for http requests
     fetch(endpoint)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => {
+        this.setState({
+          person: data
+        })
+      })
   }
 
   render() {
+    const person = this.state.person
     return (
-      null
+      <div className='text-center pt-5'>
+        {person === null ? <h1>Loading...</h1> : <Person person={person} />}
+      </div>
     )
   }
 }
