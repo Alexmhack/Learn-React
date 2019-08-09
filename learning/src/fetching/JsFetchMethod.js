bd766bda0e2f104c7427088131815e08f996861c
alexHooper <alexmohack@gmail.com>
2019-08-10 01:10:15 +0530

    use loading state until done fetch

--- learning/src/fetching/JsFetchMethod.js
+++ learning/src/fetching/JsFetchMethod.js
@@ -6,11 +6,15 @@ class JsFetchMethod extends Component {
   constructor() {
     super()
     this.state = {
+      loading: false,
       person: null
     }
   }
 
   componentDidMount() {
+    this.setState({
+      loading: true
+    })
     const endpoint = "https://swapi.co/api/people/1/"
 
     // global javascript method for http requests
@@ -18,16 +22,18 @@ class JsFetchMethod extends Component {
       .then(response => response.json())
       .then(data => {
         this.setState({
-          person: data
+          person: data,
+          loading: false
         })
       })
   }
 
   render() {
     const person = this.state.person
+
     return (
       <div className='text-center pt-5'>
-        {person === null ? <h1>Loading...</h1> : <Person person={person} />}
+        {this.state.loading ? <h1>Loading...</h1> : <Person person={person} />}
       </div>
     )
   }
