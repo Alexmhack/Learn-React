12b1d1faa11ad21eac3e775363abe4a1eab7030d
alexHooper <alexmohack@gmail.com>
2019-08-09 21:27:18 +0530

    change return based on prop

--- learning/src/lifecycles/Conditional.js
+++ learning/src/lifecycles/Conditional.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 function Conditional(props) {
-  console.log(props.isLoading)
+  if (props.isLoading === true) {
+    return (
+      <div className='text-center p-5'>
+        <h1>Temp</h1>
+      </div>
+    )
+  } else {
+    return (
+      <div className='text-center p-5'>
+        <h1>The cool stuff has been loaded successfully.</h1>
+      </div>
+    )
+  }
 
-  return (
-    <div className='text-center p-5'>
-      <h1>Temp</h1>
-    </div>
-  )
 }
 
 export default Conditional
