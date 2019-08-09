ef66b8fe17c289398b95aedd33645a8fa1e89450
alexHooper <alexmohack@gmail.com>
2019-08-10 00:34:26 +0530

    create a login button

---
+++ learning/src/classBased/LoginButton.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+
+class LoginButton extends Component {
+  constructor() {
+    super()
+    this.handleLoginButton = this.handleLoginButton.bind(this)
+
+    this.state = {
+      loggedIn: false
+    }
+  }
+
+  handleLoginButton(event) {
+    event.preventDefault()
+
+    this.setState({
+      loggedIn: !this.state.loggedIn
+    })
+  }
+
+  render() {
+    return (
+      <div className='text-center pt-5'>
+        <h1>You are {this.state.loggedIn ? "Logged In" : "Logged Out"}</h1>
+        <br />
+        <button className='btn btn-secondary' onClick={this.handleLoginButton}>
+          {this.state.loggedIn ? "Logout" : "Login"}
+        </button>
+      </div>
+    )
+  }
+}
+
+export default LoginButton
