d74dacd7d2d2aaa94c33a98f840174ed51c63e8b
alexHooper <alexmohack@gmail.com>
2019-08-09 21:33:31 +0530

    wrap jsx in div

--- learning/src/lifecycles/Conditional.js
+++ learning/src/lifecycles/Conditional.js
@@ -5,14 +5,14 @@ function Conditional(props) {
   // condition ? return if true : return if false
 
   return (
-    props.isLoading === true ?
-      <div className='text-center p-5'>
+    <div className='text-center p-5'>
+      <h1>Header</h1>
+      {props.isLoading === true ?
         <h1>Temp</h1>
-      </div>
-      :
-      <div className='text-center p-5'>
-        <h1>The cool stuff has been loaded successfully.</h1>
-      </div>
+        : <h1>The cool stuff has been loaded successfully.</h1>
+      }
+      <h1>Footer</h1>
+    </div>
   )
 
 }
