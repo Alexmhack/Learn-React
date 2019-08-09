6fc80967abe90432b1fb8b07fe0ca9f25ba740f3
alexHooper <alexmohack@gmail.com>
2019-08-10 00:55:14 +0530

    convert into class based component

--- learning/src/fetching/JsFetchMethod.js
+++ learning/src/fetching/JsFetchMethod.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function JsFetchMethod(props) {
-  const endpoint = "https://swapi.co/api/people/1/"
+class JsFetchMethod extends Component {
+  componentDidMount() {
+    const endpoint = "https://swapi.co/api/people/1/"
 
-  fetch(endpoint)
-    .then(response => response.json())
-    .then(data => console.log(data))
+    // global javascript method for http requests
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
 
-  return null
+  render() {
+    return (
+      null
+    )
+  }
 }
 
 export default JsFetchMethod
