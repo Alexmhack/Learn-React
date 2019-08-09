2c36e5c525616ef5616610f819047931f465cb5d
alexHooper <alexmohack@gmail.com>
2019-08-10 01:09:43 +0530

    check if person prop is not null

--- learning/src/fetching/Person.js
+++ learning/src/fetching/Person.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react'
 
 class Person extends Component {
   render() {
-    console.log(this.props.person)
+    const person = this.props.person
 
     return (
-      <h1>{this.props.person.name}</h1>
+      <h1>{person !== null ? this.props.person.name : null}</h1>
     )
   }
 }
