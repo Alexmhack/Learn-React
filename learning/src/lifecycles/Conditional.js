6d328526e7dfadbc00420047231cae69995a6bbf
alexHooper <alexmohack@gmail.com>
2019-08-09 21:32:08 +0530

    use ternary operator to return

--- learning/src/lifecycles/Conditional.js
+++ learning/src/lifecycles/Conditional.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
 function Conditional(props) {
-  if (props.isLoading === true) {
-    return (
+  // ternary operator
+  // condition ? return if true : return if false
+
+  return (
+    props.isLoading === true ?
       <div className='text-center p-5'>
         <h1>Temp</h1>
       </div>
-    )
-  }
-
-  return (
-    <div className='text-center p-5'>
-      <h1>The cool stuff has been loaded successfully.</h1>
-    </div>
+      :
+      <div className='text-center p-5'>
+        <h1>The cool stuff has been loaded successfully.</h1>
+      </div>
   )
 
 }
