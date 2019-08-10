ab647b2ceeab26b453bd5a565467e45037581139
alexHooper <alexmohack@gmail.com>
2019-08-10 18:48:26 +0530

    add image

--- learning/src/capstone/Header.js
+++ learning/src/capstone/Header.js
@@ -4,6 +4,10 @@ function Header(props) {
   return (
     <header>
       <h2>MEME GENERATOR</h2>
+      <img
+          src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+          alt="Problem?"
+      />
     </header>
   )
 }
