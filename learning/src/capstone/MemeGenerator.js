ad09bb3da32c54d0cbd34b4217c8da6210b9197d
alexHooper <alexmohack@gmail.com>
2019-08-10 19:17:15 +0530

    display meme with top and bottom text

--- learning/src/capstone/MemeGenerator.js
+++ learning/src/capstone/MemeGenerator.js
@@ -50,8 +50,11 @@ class MemeGenerator extends Component {
           />
           <button>Generate</button>
         </form>
-        <h2>{this.state.topText}</h2>
-        <h2>{this.state.bottomText}</h2>
+        <div className="meme">
+          <img src={this.state.randomImg} alt="Random meme" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
       </div>
     )
   }
