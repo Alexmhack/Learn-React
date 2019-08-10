c2ab2973a222f531f95118e30cf9cbf54de85d83
alexHooper <alexmohack@gmail.com>
2019-08-10 18:42:41 +0530

    display sample html

--- learning/src/capstone/Header.js
+++ learning/src/capstone/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header(props) {
   return (
     <header>
-
+      <h2>Header</h2>
     </header>
   )
 }
--- learning/src/capstone/MemeGenerator.js
+++ learning/src/capstone/MemeGenerator.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class MemeGenerator extends Component {
   render() {
     return (
-
+      <h1>MEME GENERATOR</h1>
     )
   }
 }
