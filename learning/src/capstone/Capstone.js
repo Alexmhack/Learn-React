e9345a8ac365352f0965ae48ce89742b9d104290
alexHooper <alexmohack@gmail.com>
2019-08-10 18:41:53 +0530

    render both components

---
+++ learning/src/capstone/Capstone.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react'
+
+import Header from './Header'
+import MemeGenerator from './MemeGenerator'
+
+function Capstone(props) {
+  return (
+    <Fragment>
+      <Header />
+      <MemeGenerator />
+    </Fragment>
+  )
+}
+
+export default Capstone
