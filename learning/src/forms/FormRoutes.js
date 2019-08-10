be1fd0aac2ca537aab8d993cf325975be232bb2d
alexHooper <alexmohack@gmail.com>
2019-08-10 14:53:10 +0530

    create route for booking airline ticket

--- learning/src/forms/FormRoutes.js
+++ learning/src/forms/FormRoutes.js
@@ -3,12 +3,14 @@ import {Route} from 'react-router-dom'
 
 import PartOne from './PartOne'
 import PartTwo from './PartTwo'
+import AirlineForm from './AirlineForm'
 
 function FormRoutes() {
   return (
     <Fragment>
       <Route exact path='/forms/one' component={PartOne} />
       <Route exact path='/forms/two' component={PartTwo} />
+      <Route exact path='/forms/book' component={AirlineForm} />
     </Fragment>
   )
 }
