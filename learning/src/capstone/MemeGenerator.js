4bf71dcec317d952841b12d668a95c6ad7cc89f9
alexHooper <alexmohack@gmail.com>
2019-08-10 18:48:44 +0530

    add meme properties in state

--- learning/src/capstone/MemeGenerator.js
+++ learning/src/capstone/MemeGenerator.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react'
 
 class MemeGenerator extends Component {
+  constructor() {
+    super()
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: "http://i.imgflip.com/1bij.jpg"
+    }
+  }
+
   render() {
     return (
       <h1>MEME GENERATOR</h1>
