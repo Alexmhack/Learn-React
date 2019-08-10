c75c6ac47bb5a3300b9087e8ee4b068be1f8fc46
alexHooper <alexmohack@gmail.com>
2019-08-10 14:33:34 +0530

    handle form submit using method

--- learning/src/forms/PartTwo.js
+++ learning/src/forms/PartTwo.js
@@ -20,32 +20,28 @@ class PartTwo extends Component {
     type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
   }
 
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log('form is submitted')
+  }
+
   render() {
     const {gender} = this.state
     return (
       <div className='container pt-5'>
         <div className='row'>
           <div className='col-md-6 offset-md-3'>
-            <div className="form-group mb-3">
-              <div className="mb-3">
-                <label htmlFor="textarea1">Example textarea</label>
-                <textarea
-                  className="form-control"
-                  id="textarea1"
-                  rows="3"
-                  name="description"
-                  onChange={this.handleChange}
-                  // value={"This is the sample value for text area."}
-                />
-              </div>
-              <div className="input-group-prepend">
-                <div className="input-group-text">
-                  <input
-                    type="checkbox"
-                    aria-label="Checkbox for following text input"
-                    checked={this.state.isFriendly}
-                    name="isFriendly"
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group mb-3">
+                <div className="mb-3">
+                  <label htmlFor="textarea1">Example textarea</label>
+                  <textarea
+                    className="form-control"
+                    id="textarea1"
+                    rows="3"
+                    name="description"
                     onChange={this.handleChange}
+                    // value={"This is the sample value for text area."}
                   />
                 </div>
               </div>
@@ -73,19 +69,8 @@ class PartTwo extends Component {
                   /> Female
                 </div>
               </div>
-              <div>
-                <select
-                  value={this.state.favColor}
-                  name="favColor"
-                  onChange={this.handleChange}
-                >
-                  <option value="blue">Blue</option>
-                  <option value="orange">Orange</option>
-                  <option value="red">Red</option>
-                  <option value="yellow">Yellow</option>
-                </select>
-              </div>
-            </div>
+              <button>Submit</button>
+            </form>
             <div>
               <h2>{this.state.description}</h2>
               <h2>{this.state.isFriendly}</h2>
