9f38573dd9285051dca02f8c29bf4d7299d10b20
alexHooper <alexmohack@gmail.com>
2019-08-10 08:31:17 +0530

    add a checkbox

--- learning/src/forms/PartTwo.js
+++ learning/src/forms/PartTwo.js
@@ -26,15 +26,28 @@ class PartTwo extends Component {
         <div className='row'>
           <div className='col-md-6 offset-md-3'>
             <div className="form-group mb-3">
-              <label htmlFor="textarea1">Example textarea</label>
-              <textarea
-                className="form-control"
-                id="textarea1"
-                rows="3"
-                name="description"
-                onChange={this.handleChange}
-                // value={"This is the sample value for text area."}
-              />
+              <div className="mb-3">
+                <label htmlFor="textarea1">Example textarea</label>
+                <textarea
+                  className="form-control"
+                  id="textarea1"
+                  rows="3"
+                  name="description"
+                  onChange={this.handleChange}
+                  // value={"This is the sample value for text area."}
+                />
+              </div>
+              <div className="input-group-prepend">
+                <div className="input-group-text">
+                  <input
+                    type="checkbox"
+                    aria-label="Checkbox for following text input"
+                    checked={this.state.isFriendly}
+                    name="isFriendly"
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </div>
             </div>
             <div>
               <h2>{this.state.description}</h2>
