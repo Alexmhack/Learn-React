4ab68dbcb6f2ea5225a82fa80f2ae5cde47e86be
alexHooper <alexmohack@gmail.com>
2019-08-10 08:15:40 +0530

    add value to textarea

--- learning/src/forms/PartTwo.js
+++ learning/src/forms/PartTwo.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react'
 
+// react textarea does not have a closing tag.
+// instead use value prop to set values
+
 class PartTwo extends Component {
   render() {
     return (
@@ -8,7 +11,12 @@ class PartTwo extends Component {
           <div className='col-md-6 offset-md-3'>
             <div className="form-group">
               <label htmlFor="textarea1">Example textarea</label>
-              <textarea className="form-control" id="textarea1" rows="3"></textarea>
+              <textarea
+                className="form-control"
+                id="textarea1"
+                rows="3"
+                value={"This is the sample value for text area."}
+              />
             </div>
           </div>
         </div>
