bf4e9b1b9c4e13f212c04c2f643ce2ff420c6a4b
alexHooper <alexmohack@gmail.com>
2019-08-10 08:04:32 +0530

    create route for part two

--- learning/src/forms/FormRoutes.js
+++ learning/src/forms/FormRoutes.js
@@ -2,11 +2,13 @@ import React, {Fragment} from 'react'
 import {Route} from 'react-router-dom'
 
 import PartOne from './PartOne'
+import PartTwo from './PartTwo'
 
 function FormRoutes() {
   return (
     <Fragment>
       <Route exact path='/forms/one' component={PartOne} />
+      <Route exact path='/forms/two' component={PartTwo} />
     </Fragment>
   )
 }
